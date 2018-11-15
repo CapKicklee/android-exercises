f5fb2f9b942abb014485bd1e159d3ea18f6c8b7a
Kendall Forest <kendall.forest.44@gmail.com>
2018-11-15 15:38:34 +0100

    Ajout de l'image avec Picasso

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -14,7 +15,8 @@ class LibraryActivity : AppCompatActivity() {
         val imageView = findViewById<ImageView>(R.id.downloadImageView)
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
-        Glide.with(this).load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg").into(imageView)
+        //Glide.with(this).load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg").into(imageView)
+        Picasso.get().load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg").into(imageView)
     }
 
 }
