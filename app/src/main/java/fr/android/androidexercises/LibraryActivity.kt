f753c6f8179d84a126e582139870611704c930cc
Kendall Forest <kendall.forest.44@gmail.com>
2018-11-15 15:32:03 +0100

    Ajout d'image avec Glide

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -2,6 +2,8 @@ package fr.android.androidexercises
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -9,8 +11,10 @@ class LibraryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
 
+        val imageView = findViewById<ImageView>(R.id.downloadImageView)
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        Glide.with(this).load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg").into(imageView)
     }
 
 }
