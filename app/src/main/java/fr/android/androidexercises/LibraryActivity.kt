341889dc7786d5294003f5f3e7dee839b9d818ea
Kendall Forest <kendall.forest.44@gmail.com>
2018-11-16 10:38:04 +0100

    Ajout des logs de livres

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -2,6 +2,13 @@ package fr.android.androidexercises
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.util.*
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -10,19 +17,36 @@ class LibraryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_library)
 
         // Plant logger cf. Android Timber
-        // Timber.plant(new Timber.DebugTree());
+        Timber.plant(Timber.DebugTree())
 
         // TODO build Retrofit
-
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         // TODO create a service
-
+        val service = retrofit.create(HenriPotierService::class.java)
         // TODO listBooks()
-
+        val list = service.listBooks()
         // TODO enqueue call and display book title
+        list.enqueue(object: Callback<Array<Book>> {
+            override fun onFailure(call: Call<Array<Book>>, t: Throwable) {
+                //Timber.e(t.message)
+            }
 
+            override fun onResponse(call: Call<Array<Book>>, response: Response<Array<Book>>) {
+                response.body()?.forEach {
+                    Timber.i("title : %s", it.title)
+                }
+            }
+        })
         // TODO log books
+        list.execute().body()?.forEach {
+            Timber.i("%s€", "Book : " + it.title + " - " + it.cover + " - " + it.isbn + " - " + it.price)
+        }
 
         // TODO display book as a list
+
     }
 
 }
